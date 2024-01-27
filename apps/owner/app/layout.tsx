ffdee5c592357b29f89bca4e5d6d3fd7afdec80f
bj-budhathoki <dev.bijay04@gmail.com>
2024-01-27 12:57:29 +0545

    feat: font configuration

--- apps/owner/app/layout.tsx
+++ apps/owner/app/layout.tsx
@@ -1,6 +1,7 @@
 import { AntdRegistry } from '@ant-design/nextjs-registry';
-import { StyledComponentsRegistry } from '@next-skeleton/shared';
+import { StyledComponentsRegistry, roboto_mono } from '@next-skeleton/shared';
 import './global.css';
+import { ConfigProvider } from 'antd';
 
 export const metadata = {
   title: 'Skelton',
@@ -13,10 +14,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto_mono.className}>
       <body>
         <AntdRegistry>
-          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+          <ConfigProvider
+            theme={{
+              token: {
+                fontFamily: '',
+              },
+            }}
+          >
+            <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+          </ConfigProvider>
         </AntdRegistry>
       </body>
     </html>
